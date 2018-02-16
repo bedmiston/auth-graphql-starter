23b228903f4af99e2de82a635771f13beaf55c99
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-02-16 07:42:04 -0600

    Implement logout functionality

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import currentUserQuery from "../queries/CurrentUser";
+import { Link } from "react-router";
+import logoutUserMutation from "../mutations/LogoutUser";
 
 class Header extends Component {
+  logoutHandler(event) {
+    event.preventDefault();
+
+    this.props.mutate().then(() => this.props.data.refetch());
+  }
+
   renderButtons() {
     const { loading, user } = this.props.data;
     if (loading) {
@@ -11,19 +19,39 @@ class Header extends Component {
 
     console.log(user);
     if (user) {
-      return <div>Logout</div>;
+      return (
+        <div>
+          <li>
+            <a onClick={this.logoutHandler.bind(this)}>Logout</a>
+          </li>
+        </div>
+      );
     } else {
-      return <div>You are not signed in</div>;
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
     }
   }
 
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">{this.renderButtons()}</div>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
       </nav>
     );
   }
 }
 
-export default graphql(currentUserQuery)(Header);
+export default graphql(logoutUserMutation)(graphql(currentUserQuery)(Header));
